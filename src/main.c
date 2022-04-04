bdb0b3dd62d9550b0ec6a29b0332391db521ad29
Yuerino <50553280+Yuerino@users.noreply.github.com>
2022-04-04 23:30:25 +0200

    chore: update main

--- src/main.c
+++ src/main.c
@@ -6,15 +6,28 @@
 /*   By: cthien-h <cthien-h@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 10:46:07 by emomkus           #+#    #+#             */
-/*   Updated: 2022/04/03 17:05:36 by cthien-h         ###   ########.fr       */
+/*   Updated: 2022/04/04 23:29:54 by cthien-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_print(void *str)
+static void	ft_print(void *str)
 {
 	printf("%s\n", (char *)str);
+}
+
+static void	ft_print_array(void *p)
+{
+	char	**command;
+
+	command = (char **)p;
+	printf("\n-list-\n");
+	while (*command)
+	{
+		printf("%s\n", *command);
+		command++;
+	}
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -31,13 +44,10 @@ int	main(int argc, char **argv, char **envp)
 		if (line == NULL)
 			break ;
 		add_history(line);
-		parser(data, line);
-		free(line);
-		if (data->clean_input)
-		{
-			ft_lstiter(data->clean_input, ft_print);
-			ft_lstclear(&data->clean_input, free);
-		}
+		parser(data, &line);
+		if (*data->command.commands)
+			ft_lstiter(*data->command.commands, ft_print_array);
+		ft_lstclear(data->command.commands, (void (*)(void *))free_array);
 	}
 	deconstructor(data);
 	return (0);
