7e5d9037a629ab80f0a7232a036832848cb68205
Yuerino <50553280+Yuerino@users.noreply.github.com>
2022-04-08 19:47:13 +0200

    feat: env resolver resolving only valid env

--- src/parser/env_resolver.c
+++ src/parser/env_resolver.c
@@ -6,12 +6,15 @@
 /*   By: cthien-h <cthien-h@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 14:25:13 by cthien-h          #+#    #+#             */
-/*   Updated: 2022/04/07 16:57:19 by cthien-h         ###   ########.fr       */
+/*   Updated: 2022/04/08 19:59:22 by cthien-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
 
+// Get PWD value from environment if exists
+// Otherwise try to get PWD from memory
+// Return NULL if error
 static char	*get_pwd(t_envp_data envp_data)
 {
 	char	*pwd;
@@ -30,34 +33,44 @@ static char	*get_pwd(t_envp_data envp_data)
 	return (pwd);
 }
 
+// Find environment and return its value in envp
+// In special case, it gets from memory (such as PWD)
+static char	*resolving_env(t_envp_data envp_data, char *env_name)
+{
+	if (!ft_strncmp(env_name, "PWD", 4))
+		return (get_pwd(envp_data));
+	else
+		return (get_env_value(envp_data.envp_cp, env_name));
+}
+
 // Return new allocated that replaces environment variable name with value
 // Environment variable name starts at idx of $
 // Return NULL if error
 static char	*replace_str_env(t_data *data, char *input, int idx)
 {
 	char	*new_str;
-	char	*env;
-	char	*tmp;
+	char	*env_value;
 	int		length;
 
 	length = 0;
-	while (input[idx] && (ft_isalpha(input[idx]) || input[idx] == '_'))
+	while (input[idx] && (ft_isalpha(input[idx])
+			|| ft_isdigit(input[idx]) || input[idx] == '_'))
 	{
 		idx++;
 		length++;
+		if (!(length - 1) && ft_isdigit(input[idx - 1]))
+			break ;
 	}
-	env = ft_substr(input, idx - length, length);
-	if (!env)
+	new_str = ft_substr(input, idx - length, length);
+	if (!new_str)
 		return (NULL);
-	if (!ft_strncmp(env, "PWD", 4))
-		tmp = get_pwd(data->envp_data);
-	else
-		tmp = get_env_value(*data->envp_data.envp_cp, env);
-	free(env);
-	if (!tmp)
+	env_value = resolving_env(data->envp_data, new_str);
+	free(new_str);
+	if (!env_value)
 		return (NULL);
-	new_str = str_replace_str_at(input, idx - length - 1, length + 1, tmp);
-	free(tmp);
+	new_str = str_replace_str_at(input, idx - length - 1, length + 1,
+			env_value);
+	free(env_value);
 	return (new_str);
 }
 
