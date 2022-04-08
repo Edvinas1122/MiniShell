54c08fd06574b952d137c5643e7a7bb48aaf2080
Yuerino <50553280+Yuerino@users.noreply.github.com>
2022-04-08 19:25:33 +0200

    feat: unset check for valid env and unset multiple env

--- src/builtin/unset.c
+++ src/builtin/unset.c
@@ -6,37 +6,51 @@
 /*   By: cthien-h <cthien-h@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 16:10:46 by cthien-h          #+#    #+#             */
-/*   Updated: 2022/04/06 17:38:04 by cthien-h         ###   ########.fr       */
+/*   Updated: 2022/04/08 19:24:17 by cthien-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 
+static void	unset_env(t_list *envp, char *env)
+{
+	t_list	*tmp;
+
+	while (envp)
+	{
+		if (envp->next && !ft_strncmp(envp->next->content,
+				env, ft_strlen(env)))
+		{
+			tmp = envp->next->next;
+			ft_lstdelone(envp->next, free);
+			envp->next = tmp;
+			break ;
+		}
+		envp = envp->next;
+	}
+}
+
 int	execute_unset(t_data *data, char **argv)
 {
 	char	*to_find;
-	t_list	*list;
-	t_list	*tmp;
+	int		exit_status;
 
-	if (argv[1])
+	exit_status = EXIT_SUCCESS;
+	while (*++argv)
 	{
-		list = *data->envp_data.envp_cp;
-		to_find = ft_strjoin(argv[1], "=");
+		if (!is_valid_env(*argv))
+		{
+			exit_status = EXIT_FAILURE;
+			ft_putstr_fd("minishell: unset: `", 2);
+			ft_putstr_fd(*argv, 2);
+			ft_putendl_fd("': not a valid identifier", 2);
+			continue ;
+		}
+		to_find = ft_strjoin(*argv, "=");
 		if (!to_find)
 			return (EXIT_FAILURE);
-		while (list)
-		{
-			if (list->next && !ft_strncmp(list->next->content,
-					to_find, ft_strlen(to_find)))
-			{
-				tmp = list->next->next;
-				ft_lstdelone(list->next, free);
-				list->next = tmp;
-				break ;
-			}
-			list = list->next;
-		}
+		unset_env(*data->envp_data.envp_cp, to_find);
 		free(to_find);
 	}
-	return (EXIT_SUCCESS);
+	return (exit_status);
 }
