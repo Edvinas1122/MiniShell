f6b813509e81312e395a5dc66792ce7c9b52bfea
Yuerino <50553280+Yuerino@users.noreply.github.com>
2022-04-08 19:46:37 +0200

    refactor: optimize checking valid env

--- src/utils/is_valid_env.c
+++ src/utils/is_valid_env.c
@@ -6,7 +6,7 @@
 /*   By: cthien-h <cthien-h@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 18:12:10 by cthien-h          #+#    #+#             */
-/*   Updated: 2022/04/08 18:23:18 by cthien-h         ###   ########.fr       */
+/*   Updated: 2022/04/08 19:46:14 by cthien-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ int	is_valid_env(char *str)
 	int	len;
 
 	len = 0;
-	if (str && (ft_isalpha(str[len]) || str[len] == '_'))
-		len++;
-	else
-		return (0);
-	while (str[len])
+	while (str && str[len])
 	{
 		if ((ft_isalpha(str[len]) || ft_isdigit(str[len]) || str[len] == '_'))
+		{
+			if (!len && ft_isdigit(str[len]))
+				return (0);
 			len++;
+		}
 		else
 			return (0);
 	}
